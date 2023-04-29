925de8e109200dbd35a5be357a2d582aae40d8d2
sangingeum <56298463+sangingeum@users.noreply.github.com>
2023-04-30 02:50:19 +0900

    Implement test.py
    Implement test.py

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-import torch
+from sklearn.metrics import classification_report
 from Model import *
 from utils import *
 from unsmile_dataset.UnsmileDataset import *
+from torch.utils.data import DataLoader
+from collections import deque
 import sys
 
 if __name__ == '__main__':
@@ -16,4 +18,29 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(model.parameters(), lr=0.000001)
     model_path = args[1]
     load_model(model_path, model, optimizer)
-    #
+    # Load test dataset
+    test_dataset, label_features = load_unsmile_data(train=False, return_label_features=True)
+    # Make dataloader
+    batch_size = 32
+    data_loader = DataLoader(test_dataset, batch_size=batch_size)
+    y_preds = deque()
+    ys = deque()
+    # Predict label
+    model.eval()
+    with torch.inference_mode():
+        for (X, y) in data_loader:
+            X = model.embed_texts(X)
+            y_pred = torch.round(model(X))
+            y_preds.append(y_pred)
+            ys.append(y)
+
+        y_preds = torch.cat(list(y_preds), dim=0).cpu()
+        ys = torch.cat(list(ys), dim=0).cpu()
+    # Convert
+    print(y_preds.shape)
+    print(ys.shape)
+    print(torch.max(ys))
+    print(torch.max(y_preds))
+    print(label_features)
+    print(classification_report(ys, y_preds, target_names=label_features))
+
